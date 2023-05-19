b81756bce58d6c19b5b27b68ea2d1ce2ba5985da
Urobukis <33214019+limborex@users.noreply.github.com>
2023-05-19 16:00:34 -0400

    comments added to index.js and app.js

--- back/src/index.js
+++ back/src/index.js
@@ -1,5 +1,11 @@
+/*these file just is used to lisent the port and creat a connections, all request of tipy app.get 
+must be configurate in app.js
+
+*/
+
 const server = require("./app.js");
 const { conn } = require("./db.js");
+
 
 require("dotenv").config();
 
@@ -7,7 +13,8 @@ conn
 	.sync({ force: true })
 	.then(async () => {
 		server.listen(process.env.PORT, () => {
-			console.log(`Listening at: http://localhost:${process.env.PORT}/`);
+			console.log(`Listening at: https://localhost:${process.env.PORT}/`);
 		});
 	})
 	.catch((error) => console.log(error.message));
+
