71c0745ba547238daa6ada42f4294fd32349a4d5
lvalcke <lvalcke88@gmail.com>
2023-05-20 02:50:55 -0500

    navigation done

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
+import styled from 'styled-components';
+
 const Home = () => {
   return (
-    <>
+    <StyledHome>
     <h1>Home</h1>
-    </>
+    </StyledHome>
   );
 };
+
+const StyledHome = styled.div`
+width: 100%;
+`;
 
 export default Home;
---
+++ front/src/components/WorkerForm/WorkerForm.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const WorkerForm = () => {
+  return (
+    <StyledWorkerForm>
+    <h1>Formulario datos del trabajador</h1>
+    </StyledWorkerForm>
+  );
+};
+
+const StyledWorkerForm = styled.div`
+width: 100%;
+`;
+
+export default WorkerForm;
