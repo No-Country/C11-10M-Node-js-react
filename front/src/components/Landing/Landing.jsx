6523ed49bbc904ded568c565cd38ce1908ba59e0
Urobukis <33214019+limborex@users.noreply.github.com>
2023-05-23 14:34:35 -0400

    create landing page

--- front/src/components/Landing/Landing.jsx
+++ front/src/components/Landing/Landing.jsx
@@ -4,103 +4,81 @@ import { useState } from "react";
 import { isLogin } from "../../redux/actions";
 import {useDispatch} from "react-redux"
 import swal from "sweetalert";
-
+import { useLocation, Link } from 'react-router-dom';
 const Landing = () => {
-	const [inputs, setInputs] = useState({
-		userName: "",
-		password: ""
-	})
-
-	const dispatch = useDispatch()
-
-	const handleChange = (e) => {
-		setInputs({
-			...inputs,
-			[e.target.name]: e.target.value
-		})
-	}
-
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		if(inputs?.userName && inputs?.password){
-			dispatch(isLogin(inputs))
-				.then((data)=>{
-					if(data.type==='LOGIN'){ //*returns correctly the code
-						navigate("/home")
-					}
-					else{
-						swal({
-							title: "Datos incorrectos",
-							icon: "error"
-						})
-					}
-				})
-				.catch(()=>console.log("error"))
+	
+	
+	const slides=[
+		{url:"https://i.ibb.co/5FKfTbR/01.webp",
+		description:"¡Podes cargar las incidencias correspondientes a cada empleado!"},
+		{url:"https://i.ibb.co/5K3xCch/02.webp",
+		description:"¡Acceder de manera fácil y ágil a los datos cargados!"},
+		{url:"https://i.ibb.co/yXDy71R/03.webp",
+		description:"¡Y facilitar la liquidación de haberes de los empleados de tu empresa!"},
+	  ]
+	
+	  
+	  /* funtion for create dots below of image */
+	  const dots= slides.map((slide, slideIndex)=>
+	  <div key={slideIndex} className='cursor-pointer' onClick={()=>goToSlide(slideIndex)}>
+	
+		  <img src="src/assets/circleEmpy.svg" alt="" />
+		
+	  </div>)
+	
+	   
+	
+	  const [currentImage,setCurrentImage]=useState(0)
+	  const [text,setText]=useState(0)
+	  
+	  const nextimage=(e)=>{
+		setCurrentImage(1+currentImage)
+		setText(1+text)
+		if(currentImage>=2){
+		  setCurrentImage(0)
 		}
-	}
+	  }
 
-	const navigate = useNavigate();
+	
+	   /* funtion for create dots below of image */
+	
+	   const goToSlide= (slideIndex)=>{
+		setCurrentImage(slideIndex)
+		setText(slideIndex)
+	   }
+	   const navigate = useNavigate();
 
-	// const Separator=()=>{
-
-	// }
 	return (
-		<div className="flex flex-col gap-2 pl-2">
-			<h1 className="font-bold text-3xl">Landing</h1>
-			<div className="flex gap-10">
-				<button
-					onClick={() => navigate("/home")}
-					className={stylesButton + " px-8"}
-				>
-					Ingresar
-				</button>
-				<button
-					onClick={() => navigate("/signup")}
-					className={stylesButton + " px-8"}
-				>
-					Registrarse
-				</button>
-				<button
-					onClick={() => navigate("/registerEmployee")}
-					className={stylesButton + " px-8"}
-				>
-					Agregar Empleado
-				</button>
-			</div>
-			<div className="grid place-items-center m-auto mt-12 mb-12">
-				<div className="box grid place-items-center m-auto mb-5">
-					<div className="grid grid-cols-1 place-items-center m-auto">
-						<img className="logo" src="./src/assets/logo.png"></img>
-					</div>
-
-					<form className="grid w-t font-semibold" onSubmit={handleSubmit}>
-						<hr className="line my-8" />
-						<label htmlFor="userName">Usuario</label>
-						<input className="my-2 bc" id="userName" type="text" name="userName"
-						onChange={handleChange} value={inputs?.userName}/>
-
-						<label htmlFor="password">Contraseña</label>
-						<input className="my-2 bc" id="password" type="text" name="password"
-						onChange={handleChange} value={inputs?.password}/>
-
-						<span className="text-right underline">
-							<a href="">Necesito ayuda</a>
-						</span>
-						<hr className="line mb-8 mt-4" />
-					</form>
-
-					<div className="grid grid-cols-2 gap-10 mt-5 mb-10 font-semibold w-auto">
-						<button className="btn-secondary " onClick={() => navigate("/register")}>
-							Recuperar Contraseña
-						</button>
-						<button className=" btn-primary" onClick={handleSubmit}>
-							Ingresar
-						</button>
-					</div>
+		<>
+    		<div className="grid place-items-center m-auto w-6/12 my-12">
+            <div className="box grid place-items-center h-full m-auto mb-5">
+            <div className="grid grid-cols-1 place-items-center m-auto">
+            	<div className="grid grid-cols-1 place-items-center m-auto">
+					<img className="w-3/12 mb-12 m-5" src="./src/assets/logo.png"></img>
 				</div>
-			</div>
-		</div>
+              <img className="mb-8 w-8/12" onClick={nextimage} src={slides[currentImage].url} alt="a" />
+
+              <div className='text-center font-medium mb-8 w-8/12'>
+				
+				<p className="">{slides[text].description}</p>
+				
+				</div>
+              <div className='flex justify-center mb-8'>
+                {dots}
+              </div>
+
+              <button className="mb-8 btn-primary"onClick={()=>navigate("/login") }>¡Empezar!</button>
+
+
+            </div>
+            </div>
+            </div>
+					
+    </>
 	);
 };
+
+
+
 
 export default Landing;
