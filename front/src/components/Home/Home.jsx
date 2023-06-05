7b923752b04273932abf8a68a8acc474f8a3e3cc
Urobukis <33214019+limborex@users.noreply.github.com>
2023-06-05 15:07:05 -0400

    new elements

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -1,96 +1,61 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import logo from "../../assets/logo.png";
 
 const Home = () => {
+
+  const userName = localStorage.getItem("user");
+
+  const imageButton=[
+    {url:"https://i.ibb.co/9N8sfb1/icono-banco.png",
+    name:"Mi perfil",
+    to:"/buildingForm"},
+    {url:"https://res.cloudinary.com/dalhatgbg/image/upload/v1684827018/icono_empleados_kirz6w.png",
+    name:"Mis empleados",
+    to:"/employees"},
+    {url:"https://res.cloudinary.com/dalhatgbg/image/upload/v1684827018/icono_liquidacion_npc6km.png",
+    name:"Calcular Liquidacion",
+    to:"/wageForm"},
+    {url:"https://res.cloudinary.com/dalhatgbg/image/upload/v1684827018/icono_descargas_yaqqlq.png",
+    name:"Descargar",
+    to:"/downloads"}
+
+    
+  ];
+  const btms=imageButton.map(e=>
+    <div className='grid grid-cols-1'>
+      <Link className="text-black" to={e.to}>
+      <img className='w-20 h-20 m-auto' src={e.url} alt="mi cuenta" />
+      <h5 className='mt-0.5'>{e.name}</h5> 
+      </Link>
+      
+    </div>
+    )
+  
   return (
-    <StyledHome>
-      <div className='home'>
-        <div className='menu'>
-          <img className='logo' src="https://res.cloudinary.com/dalhatgbg/image/upload/v1684566285/logo_2_qqpvpa.png" alt="Loading..." />
-          <div className='contenedoriconos' >
-            <div className='iconoHome'>
-              <Link style={{color:'black'}} to={'/buildingForm'}>
-                <img style={{width:'23%'}} className='logoImg' src="https://res.cloudinary.com/dalhatgbg/image/upload/v1684830150/icono_perfil_du8fbd.png" alt="Loading..." />
-                <h5>Mi perfil</h5>       
-              </Link>
-            </div>  
-            <div className='iconoHome'>
-              <Link style={{color:'black'}} to={'/employees'}>
-                <img className='logoImg' src="https://res.cloudinary.com/dalhatgbg/image/upload/v1684827018/icono_empleados_kirz6w.png" alt="Loading..." />
-                <h5>Mis empleados</h5>              
-              </Link>
-            </div>  
-            <div className='iconoHome'>
-              <Link style={{color:'black'}} to={'/wageForm'}>
-                <img className='logoImg' src="https://res.cloudinary.com/dalhatgbg/image/upload/v1684827018/icono_liquidacion_npc6km.png" alt="Loading..." />
-                <h5>Calcular Liquidacion</h5>     
-              </Link>
-            </div>  
-            <div className='iconoHome'>
-              <Link style={{color:'black'}} to={'/downloads'}>
-                <img className='logoImg' src="https://res.cloudinary.com/dalhatgbg/image/upload/v1684827018/icono_descargas_yaqqlq.png" alt="Loading..." />
-                <h5>Descargar</h5>     
-              </Link>
-            </div>  
-          </div>
-        </div>
+    <>
+    <div className='grid items-center md:w-6/12 m-auto text-center md:my-12 my-8 py-20 bg-white pborder'>
+      
+
+      <div className="grid grid-cols-1 place-items-center m-auto">
+				<img className="w-3/12 mb-12 m-5" src={logo}></img>
+			</div>
+
+      <div>
+        <h2>¡Hola, {userName}! Te damos la bienvenida a</h2>
       </div>
-    </StyledHome>
+
+      {/*image button*/}
+      <div className='grid md:grid-cols-2 gap-[0.5em] items-center'>
+      
+      {btms}
+
+      </div>
+    </div>
+
+    </>
   );
 };
 
-const StyledHome = styled.div`
-.home {
-  background-color: #ececf3;
-  height: 89vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  /* align-content: center; */
-  justify-content: center;
-}
-
-.menu {
-  background-color: white;
-  width: 55%;
-  height: 75%;
-  border: 10px solid rgb(20,145,94);
-  border-radius: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  align-content: center;
-}
-
-.logo {
-  width: 40%;
-}
-
-.contenedoriconos {
-  width: 82%;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;  
-  justify-content: space-evenly;
-  align-content: center;
-}
-
-.iconoHome {
-  width: 45%;
-  color: black;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  align-content: center;
-}
-
-.logoImg {
-  width: 30%;
-}
-
-`;
 
 export default Home;
--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -41,7 +41,8 @@ const NavBar = () => {
 									xmlns="http://www.w3.org/2000/svg"
 								>
 									<path
-										fill-rule="evenodd"
+                    className="w-6 h-6 absolute right-8 top-6 cursor-pointer md:hidden icon"
+										fill="currentcolor"
 										d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
 									></path>
 								</svg>
